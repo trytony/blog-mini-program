677495fd0a55f5c831851539c30f8a841c47c3fa
zmengzm <>
2019-07-29 15:31:20 +0800

    markdown

--- pages/articleDetail/articleDetail.js
+++ pages/articleDetail/articleDetail.js
@@ -1,28 +1,45 @@
 var WxParse = require('../../wxParse/wxParse.js');
 var app = getApp();
 Page({
-    data: {
-        isLoad: true,
-        articleDetail: {},
-    },
-    onLoad: function (options) {
-        var that = this;
-        wx.request({
-            url: 'https://blog.zblzm.xyz/api/mini-program/articles/' + options.id,
-            header: {
-                'content-type': 'application/json'
-            },
-            success: res => {
-                this.setData({
-                    isLoad: false,
-                    articleDetail: res.data.data,
-                });
-                var article = res.data.data.originalContent.html;
-                WxParse.wxParse('article', 'md', article, that, 5);
-                wx.setNavigationBarTitle({
-                    title: res.data.data.title
-                })
-            }
+  data: {
+    isLoad: true,
+    //article将用来存储towxml数据
+    article: {},
+    articleDetail: {},
+  },
+  onLoad: function(options) {
+    var that = this;
+    wx.request({
+      url: 'https://blog.zblzm.xyz/api/mini-program/articles/' + options.id,
+      header: {
+        'content-type': 'application/json'
+      },
+      success: res => {
+        this.setData({
+          isLoad: false,
+          articleDetail: res.data.data,
+        });
+        var article = res.data.data.originalContent.raw;
+        //将markdown内容转换为towxml数据
+        console.log(article);
+        console.log(res.data.data.originalContent);
+        //将markdown内容转换为towxml数据
+        let data = app.towxml.toJson(
+          res.data.data.originalContent.raw,               // `markdown`或`html`文本内容
+          'markdown'              // `markdown`或`html`
+        );
+
+        //设置文档显示主题，默认'light'
+        // data.theme = 'dark';
+        //设置数据
+        that.setData({
+          article: data
+        });
+        // WxParse.wxParse('article', 'html', article, that, 5);
+        wx.setNavigationBarTitle({
+          title: res.data.data.title
         })
-    },
+      }
+    })
+  },
 })
